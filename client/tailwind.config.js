558fcc09ca57bad3bfe1ea8b55c3cfc914a4e7e0
BretzKierra-FS <122029593+BretzKierra-FS@users.noreply.github.com>
2023-12-01 14:40:07 -0500

    tailwind configuration

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      lobster: ['Lobster', 'sans-serif'],
+    },
+    colors: {
+      musicRed: 'BE3144',
+      musicPurple: '22092C',
+      musicOrange: 'F05941',
+      musicViolet: '872341',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
