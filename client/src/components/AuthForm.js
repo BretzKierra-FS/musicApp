47039d0744cc2ef3f3bf3ce413c6b62327cd5805
BretzKierra-FS <122029593+BretzKierra-FS@users.noreply.github.com>
2023-12-03 15:28:59 -0500

    Update AuthForm.js

--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -1,14 +1,8 @@
 import React, { useState } from 'react';
 
 const AuthForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log('Email:', email);
-    console.log('Password:', password);
   };
 
   return (
@@ -21,32 +15,6 @@ const AuthForm = () => {
         Account
       </h2>
       <form onSubmit={handleSubmit} className="flex flex-col">
-        <div>
-          <label className="flex mb-2" htmlFor="email">
-            Email:
-          </label>
-          <input
-            className="p-1, mb-2 w-full"
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label className="flex mb-2" htmlFor="password">
-            Password:
-          </label>
-          <input
-            className="p-1, mb-2 w-full"
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
         <button
           className="bg-musicRed w-1/2 m-auto mt-2 rounded-lg p-1
           "
